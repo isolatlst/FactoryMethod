aaf62e81b4fb6681254e699bb00797b1cebf419a
Kirill <kirill.gurin20152016@yandex.ru>
2024-11-01 04:23:38 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            FactoryMethod deliveryLogistic = new CreateRoadDelivery();
+            //deliveryLogistic = new CreateSeaDelivery();
+            IDeliver concreteDeliveryVehicle = deliveryLogistic.Create();
+            concreteDeliveryVehicle.Deliver();
+        }
+
+        #region Abstractions
+            public interface IDeliver
+            {
+                void Deliver();
+            }
+        
+            public abstract class FactoryMethod
+            {
+                public abstract IDeliver Create();
+            }
+        #endregion
+        
+        #region ConcreteCreator
+            public class CreateRoadDelivery : FactoryMethod
+            {
+                public override IDeliver Create() => new Truck();
+            }
+            
+            public class CreateSeaDelivery : FactoryMethod
+            {
+                public override IDeliver Create() => new Ship();
+            }
+        #endregion
+        
+        #region ConcreteProduct
+            public class Truck : IDeliver
+            {
+                public void Deliver()
+                {
+                    Console.WriteLine("Truck Delivered");
+                }
+            }
+
+            public class Ship : IDeliver
+            {
+                public void Deliver()
+                {
+                    Console.WriteLine("Ship Delivered");
+                }
+            }
+        #endregion
+    }
+}
